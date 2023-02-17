b47a953dab494a644070d5f47154de6f1d4a03c3
bayableteshome <bayableteshome@gmail.com>
2023-02-17 08:54:37 -0800

    advanced combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print combination
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, p, q;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			for (p = n; p <= '9'; p++)
+			{
+				if (m == '0' && p != '0')
+					q = '0';
+				else
+					q = m + 1;
+				for (; q <= '9'; q++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(' ');
+					putchar(p);
+					putchar(q);
+					if (n == '9' && m == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
