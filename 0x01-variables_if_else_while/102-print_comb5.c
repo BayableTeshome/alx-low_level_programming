2694462c09f31a49561b6920575ed7175898c028
bayableteshome <bayableteshome@gmail.com>
2023-02-17 11:19:29 -0800

    advanced combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,10 +18,12 @@ int main(void)
 		{
 			for (p = n; p <= '9'; p++)
 			{
-				if ((n == '0' || m == '0') && p != '0')
+				if ((n == '0' && m == '0') && p != '0')
 					q = '0';
-				else
+				else if (n == p)
 					q = m + 1;
+				else
+					q = '0';
 				for (; q <= '9'; q++)
 				{
 					putchar(n);
